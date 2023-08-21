c5868a629a3c8644cf3a8395b2287d62b371806f
patrickroyme <patrickroyme@gmail.com>
2023-08-21 08:17:54 +0300

    @kinetix/* namespace applied.

--- test/integration-tests/shared/swapRouter02Helpers.ts
+++ test/integration-tests/shared/swapRouter02Helpers.ts
@@ -1,8 +1,8 @@
 import JSBI from 'jsbi'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
-import { BigintIsh, CurrencyAmount, Token } from '@uniswap/sdk-core'
-import { Pair } from '@uniswap/v2-sdk'
-import { encodeSqrtRatioX96, FeeAmount, nearestUsableTick, Pool, TickMath, TICK_SPACINGS } from '@uniswap/v3-sdk'
+import { BigintIsh, CurrencyAmount, Token } from '@kinetix/sdk-core'
+import { Pair } from '@kinetix/v2-sdk'
+import { encodeSqrtRatioX96, FeeAmount, nearestUsableTick, Pool, TickMath, TICK_SPACINGS } from '@kinetix/v3-sdk'
 import { getV2PoolReserves, WETH, DAI, USDC, USDT } from './mainnetForkHelpers'
 import { BigNumber } from 'ethers'
 
